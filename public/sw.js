6eaf7fc834afcbc752ef716461573275c5d9776f
PflegePro-app <randrianantenainafetra@gmail.com>
2026-05-15 11:29:40 +0200

    PWA service worker auto-update simplifié

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,15 @@
-const CACHE = 'pflegepro-v1'
-const ASSETS = [
-  '/PflegePro/',
-  '/PflegePro/index.html',
-]
-
+const CACHE = 'pflegepro-' + '2025-05-15'
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
+  self.skipWaiting()
 })
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   )
+  self.clients.claim()
+})
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
 })
