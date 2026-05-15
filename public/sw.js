bf7022aaf334e8b4b5a44f926d462237916bf4c5
PflegePro-app <randrianantenainafetra@gmail.com>
2026-05-15 18:09:48 +0200

    Fix service worker OneSignal

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,7 @@
-const CACHE = 'pflegepro-' + '2025-05-15'
-self.addEventListener('install', e => {
-  self.skipWaiting()
-})
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  )
-  self.clients.claim()
-})
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+const CACHE = 'pflegepro-v1'
+
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
 })
