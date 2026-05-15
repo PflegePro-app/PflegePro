9652824f275d3fd737cedca982a9918d9d9da03f
PflegePro-app <randrianantenainafetra@gmail.com>
2026-05-15 18:19:22 +0200

    Fix OneSignal service worker path

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,1 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-const CACHE = 'pflegepro-v1'
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
-})
